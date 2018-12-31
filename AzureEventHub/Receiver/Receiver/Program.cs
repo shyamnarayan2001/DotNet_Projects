714e56083a9e64a8e7509a2f3be9f198f2580a00
Narayan K Murthy Shyam <narayan.k.murthy.shyam@ericsson.com>
2018-12-31 15:10:02 +0530

    1) Removed the Azure API Keys in Receiver Program.cs 2) Upgraded Microsoft.Data.OData to 5.8.4 to avoid security vulnerability issue from Github 3) Deleted the binary files in Sender

--- AzureEventHub/Receiver/Receiver/Program.cs
+++ AzureEventHub/Receiver/Receiver/Program.cs
@@ -11,10 +11,10 @@ namespace Receiver
     {
         static void Main(string[] args)
         {
-            string eventHubConnectionString = "Endpoint=sb://mynarayaneventhub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=j8BsjsNaRgHuGwlX+gJ83WAwATPPkyPW8Q4V/Wj430Y=";
-            string eventHubName = "mynarayaneventhub";
-            string storageAccountName = "mynarayanehstorage";
-            string storageAccountKey = "gtgh5wxHX2x7+KKI9GOY9byAvMcRuW9puxMCDRULhmV0rqYp6YL7gvHZGY71eh7cOcqEJYqCMJZmO7vqHOI8Iw==";
+            string eventHubConnectionString = "{Event Hubs namespace connection string}";
+            string eventHubName = "{Event Hub name}";
+            string storageAccountName = "{storage account name}";
+            string storageAccountKey = "{storage account key}";
             string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
             string eventProcessorHostName = Guid.NewGuid().ToString();
             EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
